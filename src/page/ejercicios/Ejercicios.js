cf83c834759aae6b351a6b6ba716b606bdb7f827
Alexander <alexdroide466@gmail.com>
2023-10-18 19:32:47 -0500

    Llamada a la api

--- src/page/ejercicios/Ejercicios.js
+++ src/page/ejercicios/Ejercicios.js
@@ -1,11 +1,10 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from "react";
 import bnnEjercicos from "../../asset/img/ejercicios/bnnEjercicio1.jpg";
 import Navbar from "../../components/navbar/Navbar";
 import Footer from "../../components/footer/footer";
 
 function Ejercicios() {
-  const [count, setCount] = useState(undefined); // count recibe el valor del button
-  const [seleccionado, setSeleccionado] = useState(false); //estado del button
+  const [count, setCount] = useState(""); // count recibe el valor del button
   const [data, setData] = useState(); //variables de extraccion de datos de la api
 
   //conexion con la api
@@ -42,15 +41,14 @@ function Ejercicios() {
             </p>
           </div>
 
-{/* ////////////////////////////////////Buttons/////////////////////////////////////// */}
+          {/* ////////////////////////////////////Buttons/////////////////////////////////////// */}
           <div className="flex flex-col items-center gap-4">
             <div className="flex gap-3">
               <button
                 className="btnStyles bg-black focus:bg-[#EFB810] focus:text-black"
                 type="button"
                 onClick={() => {
-                  setSeleccionado(!seleccionado); //se invierte el valor de la variable a true cuando se hace click
-                  seleccionado ? setCount("adductors") : setCount(null); //si el estado de seleccionado es true se le asigna un valor a count
+                  setCount("adductors");
                 }}
               >
                 Adductor
@@ -58,12 +56,18 @@ function Ejercicios() {
               <button
                 className="btnStyles bg-black focus:bg-[#EFB810] focus:text-black"
                 type="button"
+                onClick={() => {
+                  setCount("abductors");
+                }}
               >
                 Abductors
               </button>
               <button
                 className="btnStyles bg-black focus:bg-[#EFB810] focus:text-black"
                 type="button"
+                onClick={() => {
+                  setCount("abdominals");
+                }}
               >
                 Abdominals
               </button>
@@ -71,8 +75,7 @@ function Ejercicios() {
                 className="btnStyles bg-black focus:bg-[#EFB810] focus:text-black"
                 type="button"
                 onClick={() => {
-                  setSeleccionado(!seleccionado);
-                  seleccionado ? setCount("biceps") : setCount(null);
+                  setCount("biceps");
                 }}
               >
                 Biceps
@@ -80,12 +83,18 @@ function Ejercicios() {
               <button
                 className="btnStyles bg-black focus:bg-[#EFB810] focus:text-black"
                 type="button"
+                onClick={() => {
+                  setCount("chest");
+                }}
               >
                 Chest
               </button>
               <button
                 className="btnStyles bg-black focus:bg-[#EFB810] focus:text-black"
                 type="button"
+                onClick={() => {
+                  setCount("calves");
+                }}
               >
                 Calves
               </button>
@@ -94,36 +103,54 @@ function Ejercicios() {
               <button
                 className="btnStyles bg-black focus:bg-[#EFB810] focus:text-black"
                 type="button"
+                onClick={() => {
+                  setCount("forearms");
+                }}
               >
                 Forearms
               </button>
               <button
                 className="btnStyles bg-black focus:bg-[#EFB810] focus:text-black"
                 type="button"
+                onClick={() => {
+                  setCount("glutes");
+                }}
               >
                 Glutes
               </button>
               <button
                 className="btnStyles bg-black focus:bg-[#EFB810] focus:text-black"
                 type="button"
+                onClick={() => {
+                  setCount("hamstrings");
+                }}
               >
                 Hamstrings
               </button>
               <button
                 className="btnStyles bg-black focus:bg-[#EFB810] focus:text-black"
                 type="button"
+                onClick={() => {
+                  setCount("lower_back");
+                }}
               >
                 Lower back
               </button>
               <button
                 className="btnStyles bg-black focus:bg-[#EFB810] focus:text-black"
                 type="button"
+                onClick={() => {
+                  setCount("lats");
+                }}
               >
                 Lats
               </button>
               <button
                 className="btnStyles bg-black focus:bg-[#EFB810] focus:text-black"
                 type="button"
+                onClick={() => {
+                  setCount("middle_back");
+                }}
               >
                 Middle back
               </button>
@@ -132,36 +159,55 @@ function Ejercicios() {
               <button
                 className="btnStyles bg-black focus:bg-[#EFB810] focus:text-black"
                 type="button"
+                onClick={() => {
+                  setCount("neck");
+                }}
               >
                 Neck
               </button>
               <button
                 className="btnStyles bg-black focus:bg-[#EFB810] focus:text-black"
                 type="button"
+                onClick={() => {
+                  setCount("triceps");
+                }}
               >
                 Triceps
               </button>
               <button
                 className="btnStyles bg-black focus:bg-[#EFB810] focus:text-black"
                 type="button"
+                onClick={() => {
+                  setCount("traps");
+                }}
               >
                 Traps
               </button>
               <button
                 className="btnStyles bg-black focus:bg-[#EFB810] focus:text-black"
                 type="button"
+                onClick={() => {
+                  setCount("quadriceps");
+                }}
               >
                 Quadriceps
               </button>
             </div>
-            <div className='flex flex-col p-10'>
-              <ul className='flex flex-col gap-5'>
+            <div className="flex flex-col p-10">
+              <ul className="grid grid-cols-2 gap-5">
                 {data?.map((exercise) => (
-                  <li key={exercise.id} className='bg-white text-black p-10 rounded-xl'>
+                  <li
+                    key={exercise.id}
+                    className="bg-white text-black p-10 rounded-xl"
+                  >
                     <div>
-                      <h3 className='text-[#EFB810] font-semibold text-4xl'>{exercise.name}</h3>
-                      <p className='text-xl'>{exercise.difficulty}</p>
-                      <p className='text-xl'>{exercise.equipment}</p>
+                      <h3 className="text-[#EFB810] font-semibold text-4xl">
+                        {exercise.name}
+                      </h3>
+                      <p className="text-xl">{exercise.muscle}</p>
+                      <p className="text-xl">{exercise.equipment}</p>
+                      <p className="text-xl">{exercise.difficulty}</p>
+                      <p className="text-sm">{exercise.instructions}</p>
                     </div>
                   </li>
                 ))}
