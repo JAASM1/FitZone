c296076651a9d8ef85025a465c36925bfc4f7cfc
Alexander <alexdroide466@gmail.com>
2023-11-26 23:41:11 -0500

    Ajustes en Ejerecicios

--- src/page/Inicio/Inicio.js
+++ src/page/Inicio/Inicio.js
@@ -1,83 +1,139 @@
-import React from 'react'
-import img1 from '../../asset/img/inicio/img1.jpg'
-import img2 from '../../asset/img/inicio/img2.jpg'
-import imgMovil from '../../asset/img/inicio/imgMovil.jpg'
-import imgMovil2 from '../../asset/img/inicio/imgMovil2.jpg'
+import React, { useEffect, useRef } from "react";
+import img1 from "../../asset/img/inicio/img1.jpg";
+import img2 from "../../asset/img/inicio/img2.jpg";
+import imgMovil from "../../asset/img/inicio/imgMovil.jpg";
+import imgMovil2 from "../../asset/img/inicio/imgMovil2.jpg";
 
-import Navbar from '../../components/navbar/Navbar'
-import Footer from '../../components/footer/footer'
+import Navbar from "../../components/navbar/Navbar";
+import Footer from "../../components/footer/footer";
 
-import iconEjercicio from '../../asset/icons/pesa (1).png'
-import iconNutricion from '../../asset/icons/nutricion.png'
-import iconCalorias from '../../asset/icons/calorias.png'
-import { Link } from 'react-router-dom'
+import { motion, useInView, useAnimation } from "framer-motion";
+import iconEjercicio from "../../asset/icons/pesa (1).png";
+import iconNutricion from "../../asset/icons/nutricion.png";
+import iconCalorias from "../../asset/icons/calorias.png";
+import { Link } from "react-router-dom";
 
 function Inicio() {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+  const mainControls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+    }
+  }, [isInView]);
   return (
-    <div className='text-white bg-[#333] font-Montserrat'>
-      <Navbar/>
-      {/* banner principal */}
-      <div className='flex'>
-        <div className='flex flex-col absolute md:left-24 md:bottom-[7rem] md:gap-14 left-4 pt-20 gap-5'>
-          <div className='md:text-6xl text-3xl uppercase flex flex-col md:gap-5 tracking-[10px]'>
-            <p className=' font-bold'>Transforma</p>
-            <p className='font-extralight'>Tu cuerpo</p>
+    <div ref={ref}>
+      <div className="text-white bg-[#333] font-Montserrat">
+        <Navbar />
+        {/* banner principal */}
+        <div className="flex">
+          <motion.div
+            variants={{
+              hidden: { opacity: 0, y: 75 },
+              visible: { opacity: 1, y: 0 },
+            }}
+            initial="hidden"
+            animate={mainControls}
+            transition={{ duration: 0.5, delay: 0.25 }}
+            className="flex flex-col absolute md:left-24 md:bottom-[7rem] md:gap-14 left-4 pt-20 gap-5"
+          >
+            <div className="md:text-6xl text-3xl uppercase flex flex-col md:gap-5 tracking-[10px]">
+              <p className=" font-bold">Transforma</p>
+              <p className="font-extralight">Tu cuerpo</p>
+            </div>
+            <div className="md:text-2xl text-lg font-extralight">
+              <p>
+                Vuélvete <snap className="text-[#EFB810]">fit</snap>
+              </p>
+            </div>
+            <div>
+              <Link to="/Iniciar sesion">
+                <button className="border-2 border-[#EFB810] p-2 px-5 rounded-full font-medium">
+                  Iniciar ahora
+                </button>
+              </Link>
+            </div>
+          </motion.div>
+          <div className="max-md:hidden">
+            <img src={img1} alt=""></img>
           </div>
-          <div className='md:text-2xl text-lg font-extralight'>
-            <p >Vuélvete <snap className='text-[#EFB810]'>fit</snap></p>
+          <div className="md:hidden static">
+            <img src={imgMovil} alt=""></img>
+          </div>
+        </div>
+
+        {/* Lo que ofrecemos */}
+        <div className="flex flex-col p-20 gap-20">
+          <div className="flex max-md:justify-center">
+            <p className="text-3xl font-bold uppercase max-md:text-center">
+              Lo que <br />
+              ofrecemos
+            </p>
+          </div>
+          <div className="grid md:grid-cols-3 md:gap-36 gap-5 justify-center">
+            <div className="bg-black w-[298px] h-[250px] flex flex-col justify-center items-center p-10 gap-3 rounded-3xl">
+              <img
+                src={iconEjercicio}
+                alt="icono ejercicio"
+                className="w-12"
+              ></img>
+              <p className="text-lg uppercase font-medium">Ejercicios</p>
+              <p className="text-sm font-light text-center">
+                Proporcionar información sobre ejercicios específicos dirigidos
+                a diferentes grupos musculares
+              </p>
+            </div>
+            <div className="bg-black w-[298px] h-[250px] flex flex-col justify-center items-center p-10 gap-3 rounded-3xl">
+              <img
+                src={iconNutricion}
+                alt="icono nutricion"
+                className="w-12"
+              ></img>
+              <p className="text-lg uppercase font-medium">Nutricion</p>
+              <p className="text-sm font-light text-center">
+                Ofrecer recetas y detalles nutricionales para una alimentación
+                saludable
+              </p>
+            </div>
+            <div className="bg-black w-[298px] h-[250px] flex flex-col justify-center items-center p-10 gap-3 rounded-3xl">
+              <img
+                src={iconCalorias}
+                alt="icono calorias"
+                className="w-12"
+              ></img>
+              <p className="text-lg uppercase font-medium">Calorias</p>
+              <p className="text-sm font-light text-center">
+                Calcular y mostrar la cantidad de calorías quemadas durante
+                diferentes actividades físicas
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* motivacion */}
+        <div className="flex flex-col items-end justify-center">
+          <div className="flex flex-col bg-[#EFB810] text-black md:w-[724px] md:h-[478px] w-[250px] h-[180px] md:gap-11 gap-2 justify-center items-center absolute md:right-28 right-5">
+            <p className="md:text-4xl text-xs font-bold text-center uppercase md:tracking-[10px]">
+              Nunca es tarde <br /> para empezar
+            </p>
+            <p className="text-center md:text-xl text-[10px]">
+              "El cambio comienza hoy. Cada pequeño paso
+              <br /> cuenta en tu viaje hacia una vida más saludable.
+              <br /> Con determinación, construyes un mañana más fuerte.
+              <br /> ¡Hoy, da ese primer paso hacia tu mejor versión!"
+            </p>
           </div>
           <div>
-            <Link to='/Iniciar sesion'>
-              <button className='border-2 border-[#EFB810] p-2 px-5 rounded-full font-medium'>Iniciar ahora</button>
-            </Link>
-          </div>   
-        </div>
-        <div className='max-md:hidden'>
-          <img src={img1} alt=''></img>
-        </div>
-        <div className='md:hidden static'>
-          <img src={imgMovil} alt=''></img>
-        </div>
-      </div>
-
-      {/* Lo que ofrecemos */}
-      <div className='flex flex-col p-20 gap-20'>
-        <div className='flex max-md:justify-center'>
-          <p className='text-3xl font-bold uppercase max-md:text-center'>Lo que <br/>ofrecemos</p>
-        </div>
-        <div className='grid md:grid-cols-3 md:gap-36 gap-5 justify-center'>
-          <div className='bg-black w-[298px] h-[250px] flex flex-col justify-center items-center p-10 gap-3 rounded-3xl'>
-            <img src={iconEjercicio} alt='icono ejercicio' className='w-12'></img>
-            <p className='text-lg uppercase font-medium'>Ejercicios</p>
-            <p className='text-sm font-light text-center'>Proporcionar información sobre ejercicios específicos dirigidos a diferentes grupos musculares</p>
-          </div>
-          <div className='bg-black w-[298px] h-[250px] flex flex-col justify-center items-center p-10 gap-3 rounded-3xl'>
-            <img src={iconNutricion} alt='icono nutricion' className='w-12'></img>
-            <p className='text-lg uppercase font-medium'>Nutricion</p>
-            <p className='text-sm font-light text-center'>Ofrecer recetas y detalles nutricionales para una alimentación saludable</p>
-          </div>
-          <div className='bg-black w-[298px] h-[250px] flex flex-col justify-center items-center p-10 gap-3 rounded-3xl'>
-            <img src={iconCalorias} alt='icono calorias' className='w-12'></img>
-            <p className='text-lg uppercase font-medium'>Calorias</p>
-            <p className='text-sm font-light text-center'>Calcular y mostrar la cantidad de calorías quemadas durante diferentes actividades físicas</p>
+            <img src={img2} alt="" className="max-md:hidden" />
+            <img src={imgMovil2} alt="" className="md:hidden" />
           </div>
         </div>
+        <Footer />
       </div>
-
-      {/* motivacion */}
-      <div className='flex flex-col items-end justify-center'>
-        <div className='flex flex-col bg-[#EFB810] text-black md:w-[724px] md:h-[478px] w-[250px] h-[180px] md:gap-11 gap-2 justify-center items-center absolute md:right-28 right-5'>
-          <p className='md:text-4xl text-xs font-bold text-center uppercase md:tracking-[10px]'>Nunca es tarde <br/> para empezar</p>
-          <p className='text-center md:text-xl text-[10px]'>"El cambio comienza hoy. Cada pequeño paso<br/> cuenta en tu viaje hacia una vida más saludable.<br/> Con determinación, construyes un mañana más fuerte.<br/> ¡Hoy, da ese primer paso hacia tu mejor versión!"</p>
-        </div>
-        <div>
-          <img src={ img2 } alt='' className='max-md:hidden'/>
-          <img src={ imgMovil2 } alt='' className='md:hidden'/>
-        </div>
-      </div>
-    <Footer/>
     </div>
-  )
+  );
 }
 
-export default Inicio
+export default Inicio;
--- src/page/ejercicios/Ejercicios.js
+++ src/page/ejercicios/Ejercicios.js
@@ -1,33 +1,59 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import axios from "axios";
+
 import bnnEjercicos from "../../asset/img/ejercicios/bnnEjercicio1.jpg";
 import bnnMovilEjercicos from "../../asset/img/ejercicios/bnnMovil.jpg";
+
 import Navbar from "../../components/navbar/Navbar";
 import Footer from "../../components/footer/footer";
-
 import { useAuth } from "../../Auth";
+
+import { motion, useInView, useAnimation } from "framer-motion";
 import Swal from "sweetalert2";
 
 function Ejercicios() {
   const [count, setCount] = useState(""); // variables recibe el valor del button
   const [data, setData] = useState(); //variables de extraccion de datos de la api
-  const [showMore, setShowMore] = useState(false); //variables para la funcionalidad Mostrar más/Mostrar menos
+
+  const [selectedType, setSelectedType] = useState("");
+  const [selectedDifficulty, setSelectedDifficulty] = useState("");
+  const [filtered, setFiltered] = useState([]);
+
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+  const mainControls = useAnimation();
 
   const { isLoggedIn } = useAuth();
 
   useEffect(() => {
-    if (isLoggedIn) {
-      fetch(`https://api.api-ninjas.com/v1/exercises?muscle=${count}`, {
-        method: "GET",
-        headers: {
-          "X-Api-Key": "wHcDfLI1fmto6kZltETAlw==P3Nn9u6EhOKG5MfL",
-        },
-      })
-        .then((response) => response.json())
-        .then((data) => setData(data));
+    if (isInView) {
+      mainControls.start("visible");
     }
-  }, [count, isLoggedIn]);
+  }, [isInView]);
 
+  useEffect(() => {
+    if (isLoggedIn) {
+      fetch(
+        `https://api.api-ninjas.com/v1/exercises?muscle=${count}&type=${selectedType}&difficulty=${selectedDifficulty}`,
+        {
+          method: "GET",
+          headers: {
+            "X-Api-Key": "wHcDfLI1fmto6kZltETAlw==P3Nn9u6EhOKG5MfL",
+          },
+        }
+      )
+        .then((response) => response.json())
+        .then((data) => setFiltered(data));
+    }
+  }, [count, selectedType, selectedDifficulty, isLoggedIn]);
+
+  const handleTypeChange = (event) => {
+    setSelectedType(event.target.value);
+  };
+
+  const handleDifficultyChange = (event) => {
+    setSelectedDifficulty(event.target.value);
+  };
   const handleButtonClick = async (muscle) => {
     if (isLoggedIn) {
       setCount(muscle);
@@ -56,15 +82,24 @@ function Ejercicios() {
     <>
       <Navbar />
       <div className="font-Montserrat bg-[#333333] text-white">
-        <div className="flex items-center">
-          <div className="flex flex-col absolute md:left-48 max-md:ml-10">
+        <div ref={ref} className="flex items-center">
+          <motion.div
+            variants={{
+              hidden: { opacity: 0, y: 75 },
+              visible: { opacity: 1, y: 0 },
+            }}
+            initial="hidden"
+            animate={mainControls}
+            transition={{ duration: 0.5, delay: 0.25 }}
+            className="flex flex-col absolute md:left-48 max-md:ml-10 gap-2"
+          >
             <p className="md:text-[3.5rem] text-2xl font-semibold">
               Bienvenido a tú guía de
             </p>
             <p className="md:text-7xl text-4xl text-[#EFB810] font-extrabold uppercase">
               Ejercicios
             </p>
-          </div>
+          </motion.div>
           <div className="max-md:hidden">
             <img src={bnnEjercicos} alt="ejercicios" />
           </div>
@@ -72,11 +107,17 @@ function Ejercicios() {
             <img src={bnnMovilEjercicos} alt="ejercicios" />
           </div>
         </div>
-
+        <div className="bg-black h-[10rem] p-5">
+          <p className="text-white text-2xl line-clamp-2 text-center font-semibold">
+            Aquí obtendrás información sobre los ejercicios que puedes realizar
+            <br /> por cada músculo de tu cuerpo
+          </p>
+        </div>
         <div className="flex flex-col items-center mt-10">
           <div>
             <p className="md:text-4xl text-lg max-md:text-center font-semibold mb-12">
-              Selecciona el músculo que deseas ejercitar
+              Selecciona un músculo para obtener información sobre los
+              ejercicios
             </p>
           </div>
 
@@ -232,10 +273,49 @@ function Ejercicios() {
                 Quadriceps
               </button>
             </div>
+
+            {/* Selectores de tipo y dificultad */}
+            {isLoggedIn && (
+              <div className="flex gap-5">
+                <div className="space-x-2">
+                  <label htmlFor="equipment" className="text-xl">
+                    Type:
+                  </label>
+                  <select
+                    id="equipment"
+                    value={selectedType}
+                    onChange={handleTypeChange}
+                    className="bg-zinc-900 text-lg outline-[#EFB810] rounded-xl px-2 py-1 text-center"
+                  >
+                    <option value="">All</option>
+                    <option value="strength">Strength</option>
+                    <option value="stretching">Stretching</option>
+                    <option value="powerlifting">Powerlifting</option>
+                  </select>
+                </div>
+                <div className="space-x-2">
+                  <label htmlFor="difficulty" className="text-xl">
+                    Difficulty:
+                  </label>
+                  <select
+                    id="difficulty"
+                    value={selectedDifficulty}
+                    onChange={handleDifficultyChange}
+                    className="bg-zinc-900 text-lg outline-[#EFB810] rounded-xl px-3 py-1 text-center"
+                  >
+                    <option value="">All</option>
+                    <option value="beginner">Beginner</option>
+                    <option value="intermediate">Intermediate</option>
+                    <option value="expert">Expert</option>
+                  </select>
+                </div>
+              </div>
+            )}
+
             {/* Mapeo de data */}
             <div className="flex flex-col p-10">
               <ul className="md:grid md:grid-cols-2 gap-5 max-md:space-y-5">
-                {data?.map((exercise) => (
+                {filtered?.map((exercise) => (
                   <li
                     key={exercise.id}
                     className={`bg-zinc-600 text-white capitalize md:p-10 p-5 rounded-3xl md:text-justify`}
@@ -245,11 +325,13 @@ function Ejercicios() {
                         {exercise.name}
                       </h3>
                       <p className="text-xl">{exercise.muscle}</p>
+                      <p className="text-xl">{exercise.type}</p>
                       <p className="text-xl">{exercise.equipment}</p>
                       <p className="text-xl">{exercise.difficulty}</p>
                       <p className="text-sm text-justify normal-case">
                         {exercise.instructions}
                       </p>
+                      {/* {console.log(exercise.name,exercise.instructions)} */}
                     </div>
                   </li>
                 ))}
